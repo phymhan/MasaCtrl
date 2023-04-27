f9d29088ee32a07da1b8bf3dc014526f17885bb3
Ligong Han <hanligong@gmail.com>
2023-04-27 01:08:09 -0400

    sh

---
+++ real_image_editing.sh
@@ -0,0 +1,7 @@
+# download image from https://unsplash.com/photos/YEPDV3T8Vi8
+
+python playground.py --model_path "runwayml/stable-diffusion-v1-5" --image_real andrik-langfield-YEPDV3T8Vi8-unsplash.jpg --inv_scale 1 --scale 5 --prompt1 "photo of a grey Beetle car" --prompt2 "photo of a grey Mustang car" --inv_prompt src
+python playground.py --model_path "runwayml/stable-diffusion-v1-5" --image_real andrik-langfield-YEPDV3T8Vi8-unsplash.jpg --inv_scale 1 --scale 5 --prompt1 "photo of a grey Beetle car" --prompt2 "photo of a grey Mustang car" --inv_prompt tar
+
+python playground.py --model_path "runwayml/stable-diffusion-v1-5" --image_real andrik-langfield-YEPDV3T8Vi8-unsplash.jpg --inv_scale 1 --scale 5 --prompt1 "photo of a grey Beetle car" --prompt2 "photo of a grey Beetle car in Lego style" --inv_prompt src
+python playground.py --model_path "runwayml/stable-diffusion-v1-5" --image_real andrik-langfield-YEPDV3T8Vi8-unsplash.jpg --inv_scale 1 --scale 5 --prompt1 "photo of a grey Beetle car" --prompt2 "photo of a grey Beetle car in Lego style" --inv_prompt tar
